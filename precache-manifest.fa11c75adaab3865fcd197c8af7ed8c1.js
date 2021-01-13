294db851e64c07cc292d4a2d6c1db151b4030470
mrozowski <mrozowskion@gmail.com>
2021-01-13 17:28:49 +0100

    Updates

---
+++ precache-manifest.fa11c75adaab3865fcd197c8af7ed8c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accfa7a72443a6b0aade26d5719199c6",
+    "url": "/react_website/index.html"
+  },
+  {
+    "revision": "735e5ee47659c1bf47fd",
+    "url": "/react_website/static/css/main.16dda253.chunk.css"
+  },
+  {
+    "revision": "61a2b97ec21f1aa5a7c9",
+    "url": "/react_website/static/js/2.c5cd6474.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_website/static/js/2.c5cd6474.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "735e5ee47659c1bf47fd",
+    "url": "/react_website/static/js/main.4c917393.chunk.js"
+  },
+  {
+    "revision": "9348b8e5f9bb8b71982e",
+    "url": "/react_website/static/js/runtime-main.1ce3277d.js"
+  },
+  {
+    "revision": "ff21befc36035fd9674d80e1da4b8c68",
+    "url": "/react_website/static/media/logoMain.ff21befc.png"
+  }
+]);
